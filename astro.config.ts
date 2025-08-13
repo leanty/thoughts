fc4cc9c4c6b61bbc869e3ac3e6ba63e3ed9d2ac0
lty <908977590@qq.com>
2025-08-13 17:01:57 +0800

    Update site URL to leanty.github.io for user pages

--- astro.config.ts
+++ astro.config.ts
@@ -4,7 +4,7 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
-  site: "https://esafev.com",
+  site: "https://leanty.github.io",
   output: "static",
   integrations: [sitemap(), mdx()]
 });
