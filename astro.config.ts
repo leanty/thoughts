12b1f66b7bafc6eb02096def4b6032dc40d3bbf5
lty <908977590@qq.com>
2025-08-13 19:50:03 +0800

    dis

--- astro.config.ts
+++ astro.config.ts
@@ -2,9 +2,21 @@ import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 export default defineConfig({
   site: "https://leanty.github.io",
   output: "static",
-  integrations: [sitemap(), mdx()]
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
+  integrations: [
+    sitemap(),
+    mdx({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
+  ],
 });
