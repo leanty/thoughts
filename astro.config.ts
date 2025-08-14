2f4e7d267cc5976f187ab24fa2bf48ddf5992de0
lty <908977590@qq.com>
2025-08-14 17:22:11 +0800

    color

--- astro.config.ts
+++ astro.config.ts
@@ -1,22 +1,30 @@
 import { defineConfig } from "astro/config";
-
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
+import expressiveCode from "astro-expressive-code";
 
 export default defineConfig({
   site: "https://leanty.github.io",
   output: "static",
-  markdown: {
-    remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex],
-  },
   integrations: [
     sitemap(),
+
+    //     expressiveCode({
+    //   themes: ['github-light'],
+    //   // 移除 plugins 数组
+    // }),
+
     mdx({
       remarkPlugins: [remarkMath],
       rehypePlugins: [rehypeKatex],
+      shikiConfig: {
+        theme: 'github-light',
+      },
     }),
+
   ],
 });
+
+
